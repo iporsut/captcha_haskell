6904cbef575cea12dcc3d0c20ee3104dc16ab9ee
Weerasak Chongnguluam <singpor@gmail.com>
2016-03-14 17:25:27 +0700

    Extract random function

--- captcha.hs
+++ captcha.hs
@@ -2,17 +2,19 @@ import System.Random
 import Control.Monad
 
 data CaptchaType = TextNumber | NumberText deriving(Enum)
-
 data Operand = Zero | One | Two | Three | Four | Five | Six | Seven | Eight | Nine deriving(Enum, Show)
-
 data Operator = Add | Sub | Mul deriving(Enum)
+data Captcha  = Captcha CaptchaType Operand Operator Operand
+
+toCaptchaType n = toEnum n :: CaptchaType
+toOperand n = toEnum n :: Operand
+toOperator n = toEnum n :: Operator
 
 instance Show Operator where
-   show Add = "+"
-   show Sub = "-"
-   show Mul = "*"
-
-data Captcha  = Captcha CaptchaType Operand Operator Operand
+  show op = case op of
+    Add -> "+"
+    Sub -> "-"
+    Mul -> "*"
 
 instance Show Captcha where
   show (Captcha TextNumber a operator b) = (show a) ++ " " ++ (show operator) ++ " " ++ (show $ fromEnum b)
@@ -23,11 +25,15 @@ result (Captcha _ l Add r) = fromEnum l + fromEnum r
 result (Captcha _ l Sub r) = fromEnum l - fromEnum r
 result (Captcha _ l Mul r) = fromEnum l * fromEnum r
 
+randomCaptchaType = liftM toCaptchaType $ randomRIO (0,1)
+randomOperand = liftM toOperand $ randomRIO (0,9)
+randomOperator = liftM toOperator $ randomRIO (0,2)
+
 generateCaptcha :: IO Captcha
 generateCaptcha = do
-                    captchaType <- liftM (\n -> toEnum n :: CaptchaType) $ randomRIO (0,1)
-                    operator <- liftM (\n -> toEnum n :: Operator) $ randomRIO (0,2)
-                    left <- liftM (\n -> toEnum n :: Operand) $ randomRIO (0,9)
-                    right <- liftM (\n -> toEnum n :: Operand) $ randomRIO (0,9)
+                    captchaType <- randomCaptchaType
+                    left <- randomOperand
+                    operator <- randomOperator
+                    right <- randomOperand
 
                     return $ Captcha captchaType left operator right
